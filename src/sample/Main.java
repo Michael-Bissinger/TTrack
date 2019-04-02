1e8c7673fea440117fb5b3e5ee335ffdbde35dfa
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-02 12:05:56 +0200

    Add labels

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root = new GridPane();
+
+        Label mid = new Label("Mid:");
+        Label eve = new Label("Eve");
+        
+
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+
+
     }
 
 
