fe69a1bf7db00e8bc586ff8c660f02d604c62c01
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-02 12:33:05 +0200

    Add controls to gridPane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.awt.*;
+
 
 public class Main extends Application {
 
@@ -15,8 +18,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        GridPane grid = new GridPane();
-
+        // Add controls
         Label title = new Label("Track it!");
         Label mid = new Label("Mid:");
         Label eve = new Label("Eve");
@@ -27,12 +29,20 @@ public class Main extends Application {
         TextField midtime = new TextField();
         TextField evetime = new TextField();
 
-        Scene scene = new Scene(grid, 400, 400)
+        // Add to GridPane
+        GridPane grid = new GridPane();
+        grid.add(title, 0, 0);
+        grid.add(mid, 0,1);
+        grid.add(midtime, 1, 1);
+        grid.add(midstart,1,2);
+        grid.add(eve,0,4);
+        grid.add(evetime,1,4);
+        grid.add(evestart,1,5);
 
-        grid.add(midstart, 0, 1);
+        // Scene
+        Scene scene = new Scene(grid, 400, 400);
 
-
-
+        // Stage
         primaryStage.setTitle("TTrack");
         primaryStage.setScene(scene);
         primaryStage.show();
