a5940ef8a6449b3bc8acbeef0e1b610c6280486a
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-08 14:03:41 +0200

    Add wait-time

--- src/sample/Action.java
+++ src/sample/Action.java
@@ -1,8 +1,16 @@
 package sample;
 
+import java.util.concurrent.TimeUnit;
+
 public class Action {
 
-        public static void start (String daytime, String waittime) {
+        public static void start (String daytime, int waittime) throws InterruptedException {
+
+                waittime = 3; // just for testing
+                TimeUnit.SECONDS.sleep(waittime);
+
+                
+
 
 
 
