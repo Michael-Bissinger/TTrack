aa6d5629a09f831de72056e46f6e6155036577ac
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-08 14:34:00 +0200

    Functionality plan added

--- src/sample/Action.java
+++ src/sample/Action.java
@@ -6,9 +6,15 @@ public class Action {
 
         public static void start (String daytime, int waittime) throws InterruptedException {
 
-                waittime = 3; // just for testing
+                //waittime = 3; // just for testing
                 TimeUnit.SECONDS.sleep(waittime);
                 System.out.println("I waited");
+
+
+                if (daytime.equals("eve")) {
+                        // Shut down pc and end this program
+                        System.out.println("I am shutting down the pc");
+                }
 
 
 
